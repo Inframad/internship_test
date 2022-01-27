90ffc4466a513f099dc4fe443453445d9c65311c
Тюкавкин Алексей Григорьевич <infinsinity@gmail.com>
2022-01-28 03:21:50 +0700

    Добавил viewBinding

--- app/src/main/java/com/example/internshiptest/MainActivity.kt
+++ app/src/main/java/com/example/internshiptest/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.internshiptest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.internshiptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
